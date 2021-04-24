559216df4922207c21a672a3bf0fce32af74a949
Chaojie Wu <wuchaojie925@gmail.com>
2021-04-24 23:06:39 +0800

    how to handle interface and third party library with module annotated with Binds and Provides

--- app/src/main/java/com/example/learninghilt/MainActivity.kt
+++ app/src/main/java/com/example/learninghilt/MainActivity.kt
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.google.gson.Gson
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.scopes.ActivityScoped
 import dagger.hilt.android.scopes.FragmentScoped
 import javax.inject.Inject
@@ -21,17 +26,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //println(someClass.doAThing())
+        println(someClass.doAThing())
     }
 }
 
 class SomeClass
 @Inject
 constructor(
+    private val someInterfaceImpl: SomeInterface,
     private val gson: Gson
-    //private val someInterfaceImpl: SomeInterface
 ) {
-    //fun doAThing() = "Look I got: ${someInterfaceImpl.getAThing()}"
+    fun doAThing() = "Look I got: ${someInterfaceImpl.getAThing()}"
 }
 
 class SomeInterfaceImpl
@@ -43,4 +48,23 @@ constructor() : SomeInterface {
 
 interface SomeInterface {
     fun getAThing(): String
+}
+
+
+@InstallIn(ActivityComponent::class)
+@Module
+abstract class MyModule {
+
+    @Binds
+    abstract fun bindSomeDependency(someInterfaceImpl: SomeInterfaceImpl): SomeInterface
+}
+
+@InstallIn(ActivityComponent::class)
+@Module
+object MySecondModule {
+
+    @Provides
+    fun provideGson(): Gson {
+        return Gson()
+    }
 }
